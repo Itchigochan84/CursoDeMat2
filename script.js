5ed6079a19638749745c744b883c3142949caa74
Itchigochan84 <namika.ynkn48@gmail.com>
2026-09-11 18:45:23 -0300

    Add initial quiz structure with questions and options

---
+++ script.js
@@ -0,0 +1,96 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+  // pergunta1
+  {
+      enunciado: "Você fez a isenção e inscrição para o ENEM, o que você pretende fazer agora?",
+      alternativas: [
+  {
+          texto: "Vou estudar bem o que aprendeu na escola e realizar as tarefas",
+          afirmacao: 
+          [
+            "Você começou a tirar notas maravilhosas nas disciplinas e ganhou certificado",
+            "Você começou a sentir que está entendendo as aulas na escola",
+          ],
+  },
+  {
+          texto: "Além dos estudos para as provas, vou estudar nos sites e nas plataformas, ou seja, treinando as questões que já caiu no ENEM",
+          afirmacao: 
+          [
+            "No começo, sentiu a dificuldade de entender a questão do estilo ENEM, mas de pouco em pouco começou a conseguir melhor",
+            "",
+          ],
+  },
+      ],
+  },
+  // pergunta2
+  {
+      enunciado: "perguntas",
+      alternativas: [
+  {
+          texto: "o que voce escolhe",
+          afirmacao: 
+          [
+            "o que acontece diante da escolha",
+            "",
+          ],
+  },
+  {
+          texto: "o que voce escolheu",
+          afirmacao: 
+          [
+            "o que acontece diante da escolhaa",
+            "",
+          ],
+  },
+      ],
+  },
+  // pergunta3
+  {
+      enunciado: "perguntas",
+      alternativas: [
+  {
+          texto: "o que voce escolhe",
+          afirmacao: 
+          [
+            "o que acontece diante da escolha",
+            "",
+          ],
+  },
+  {
+          texto: "o que voce escolheu",
+          afirmacao: 
+          [
+            "o que acontece diante da escolhaa",
+            "",
+          ],
+  },
+      ],
+  },
+  // pergunta4
+  {
+      enunciado: "perguntas",
+      alternativas: [
+  {
+          texto: "o que voce escolhe",
+          afirmacao: 
+          [
+            "o que acontece diante da escolha",
+            "",
+          ],
+  },
+  {
+          texto: "o que voce escolheu",
+          afirmacao: 
+          [
+            "o que acontece diante da escolhaa",
+            "",
+          ],
+  },
+      ],
+  },
+];
