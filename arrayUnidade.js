20dfd986465ae1d11b985935372612e90531e420
Itchigochan84 <namika.ynkn48@gmail.com>
2026-09-12 19:11:12 -0300

    Add unidadesEstudo array with study subjects

--- arrayUnidade.js
+++ arrayUnidade.js
@@ -1,1 +1,6 @@
-
+const unidadesEstudo = [
+    "Matemática e suas Tecnologias",
+    "Ciências da Natureza e suas Tecnologias",
+    "Linguagens, Códigos e suas Tecnologias",
+    "Ciências Humanas e Sociais Aplicadas"
+];
